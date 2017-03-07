fcabf05720ee5d16dcd870ee349b45ff6d52f22f
robojones <jonathan.scholz-weil@t-online.de>
2017-03-07 16:08:40 +0100

    use mkdirp

--- createPath.js
+++ createPath.js
@@ -1,28 +1,15 @@
-const path = require('path');
-const fs = require('fs');
+const mkdirp = require('mkdirp')
 
 module.exports = createPath;
 
-function createPath(wd, pth) {
-    return new Promise((resolve, reject) => {
-        const cwd = Array.isArray(wd) ? wd.concat() : wd.split(path.sep);
-        const parts = Array.isArray(pth) ? pth.concat() : pth.split(path.sep);
-        createDir();
-
-        function createDir() {
-            const p = parts.shift();
-            fs.mkdir(path.join(cwd.join(path.sep), p), (err) => {
-                if(err && err.code !== 'EEXIST') {
-                    reject(err);
-                    return;
-                }
-                cwd.push(p);
-                if(parts.length) {
-                    process.nextTick(createDir);
-                } else {
-                    resolve(cwd.join(path.sep));
-                }
-            });
-        }
-    });
+function createPath(dir, opts, cb) {
+  return new Promise((resolve, reject) => {
+    mkdirp(dir, opts, (err, made) => {
+      if(err) {
+        reject(err)
+      } else {
+        resolve(made)
+      }
+    })
+  })
 }
